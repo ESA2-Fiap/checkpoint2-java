5bec2991ee54ee508f6d5d3d449cf4d0111e13f1
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-04-16 18:33:17 -0300

    feat: adding attributes and methods to model Aluno

--- src/br/com/fiap/Aluno.java
+++ src/br/com/fiap/Aluno.java
@@ -1,4 +1,65 @@
 package br.com.fiap;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Aluno {
+    private int rm;
+    private String nome;
+    private LocalDate dataDeNascimento;
+
+    public Aluno() {
+    }
+
+    public Aluno(int rm, String nome, LocalDate dataDeNascimento) {
+        this.rm = rm;
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public int getRm() {
+        return rm;
+    }
+
+    public void setRm(int rm) {
+        try {
+            if (rm >= 95001 && rm <= 552999) {
+                this.rm = rm;
+            } else {
+                throw new Exception("Atributo rmdeve ser maior ou igual a 95001e menor ou igual a 552999");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        int anoDeNascimento = dataDeNascimento.getYear();
+        try {
+            if (anoDeNascimento >= 1900 && anoDeNascimento <= LocalDate.now().getYear()) {
+                this.dataDeNascimento = dataDeNascimento;
+            } else {
+                throw new Exception("Atributo dataDeNascimentodeve ser maior ou igual a 1900 e menor ou igual ao ano atual.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String calculaIdadeCompleta(LocalDate dataAtual) {
+        Period dataAtualParaIdade = Period.between(dataAtual, this.dataDeNascimento);
+        return "Idade Completa\n" + "Anos: " + dataAtualParaIdade.getYears() + "\nMeses: " + dataAtualParaIdade.getMonths() + "\nDias: " + dataAtualParaIdade.getDays();
+    }
 }
